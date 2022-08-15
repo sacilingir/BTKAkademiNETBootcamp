1810a3adb500647e02c255d7de67e06049984564
seyit ahmet cilingir <seyitacilingir@gmail.com>
2022-08-15 15:01:09 +0300

    Person has been denined.

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ console.log("Hello world!");
 
 document.getElementById("root").innerHTML='Merhaba';
 
+const arr=[
+            
+            {
+            id:1,
+            name:"Sude Akgün",
+            age:19,},
+            {
+            id:2,
+            name:"Ahmet Güneş",
+            age:23,
+            },
+            {
+             id:3,
+            name:"Ömer Dağ",
+            age:22,
+            }];
 
-let i=1;
-while (i<10) {
+for (let person of arr) {
     const element=document.createElement("div");
-    element.innerHTML=i;
+    element.innerHTML=`${person.id} ${person.name} ${person.age}`;
     document.getElementById("root").append(element);
-    console.log(i);
-    i=i+1;
+    
+    
 }
 
 
